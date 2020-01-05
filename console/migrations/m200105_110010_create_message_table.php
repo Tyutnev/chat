3882a657ad4641d8fa8638ef8f8d64c5b08e6305
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-05 19:10:26 +0800

    Added migrations

---
+++ console/migrations/m200105_110010_create_message_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%message}}`.
+ */
+class m200105_110010_create_message_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%message}}', [
+            'id' => $this->primaryKey(),
+            'id_sender' => $this->integer()->notNull(),
+            'id_recipient' => $this->integer()->notNull(),
+            'content' => $this->text()->notNull(),
+            'created_at' => 'datetime DEFAULT NOW()',
+            'status' => $this->integer(1)->defaultValue(1)
+        ]);
+
+        $this->addForeignKey(
+            'fk-message-id_sender',
+            'message',
+            'id_sender',
+            'user',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-message-id_recipient',
+            'message',
+            'id_recipient',
+            'user',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%message}}');
+    }
+}
