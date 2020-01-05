e77397e4cd2b289dcd47682f809f54c5b366b537
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-05 21:36:22 +0800

    Added login.js

---
+++ frontend/web/js/login.js
@@ -0,0 +1,27 @@
+$('.message a').click(function() {
+    $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
+
+$('.ajax-form').on('beforeSubmit', (event) => {
+    $('.form-message').html('');
+
+    let data = $('.ajax-form');
+
+    $.ajax({
+        type: data.attr('method'),
+        url: $('.ajax-form:visible').attr('action'),
+        data: data.serialize(),
+        success: (html) => {
+            html = JSON.parse(html);
+
+            if (html.status == 'success') {
+                console.log('Success');
+                return;
+            }
+
+            $('.form-message').html(html.errors[Object.keys(html.errors)[0]]);
+        }
+    });
+
+    return false;
+});
