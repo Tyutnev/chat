cfaf3aa30d7689faa7006f313c46a5c3d829947f
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-07 05:47:12 +0800

    Added logic for handshake and follow-push

--- console/daemon/WebSocketServer.php
+++ console/daemon/WebSocketServer.php
@@ -4,6 +4,8 @@ namespace console\daemon;
 
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
+use common\models\User;
+use common\models\Follow;
 
 /**
  * Веб-сокет сервер
@@ -18,13 +20,48 @@ class WebSocketServer implements MessageComponentInterface
 
     public function onOpen(ConnectionInterface $conn) {
         $this->clients->attach($conn);
+
+        $conn->send(json_encode([
+            'header' => 'handshake',
+            'resource' => $conn->resourceId
+        ]));
+
         echo "Open, user has id: $conn->resourceId\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        foreach ($this->clients as $client) {
-            if ($from != $client) {
-                $client->send($msg);
+        $msg = json_decode($msg);
+
+        /**
+         * Получение идентификатора пользователя после подключения
+         */
+        if($msg->header == 'handshake' && 
+           $from->resourceId == $msg->resourceId
+        )
+        {
+            $from->id_user = User::findIdByAuthKey($msg->hash);
+        }
+
+        /**
+         * Добавление в друзья
+         */
+        if($msg->header == 'follow')
+        {
+            $model = new Follow();
+            $model->id_sender = $from->id_user;
+            $model->id_recipient = $msg->id_user;
+            $model->status = Follow::STATUS_ORDER;
+            $model->save();
+
+            foreach($this->clients as $client)
+            {
+                if($client->id_user == $msg->id_user)
+                {
+                    $client->send(json_encode([
+                        'header' => 'follow-push',
+                        'from' => User::findById($from->id_user)->username
+                    ]));
+                }
             }
         }
     }
