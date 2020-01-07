bf98da6af404028ba24fda869af93b0dbd88043c
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-07 17:45:56 +0800

    Added confirmFollowHandler

--- frontend/web/js/profile.js
+++ frontend/web/js/profile.js
@@ -1,3 +1,24 @@
+/**
+ * Обработчик события подтверждения заявки в друзья
+ */
+const confirmFollowHandler = (event) => {
+    let button = $(event.target);
+    let container = button;
+
+    while (!container.attr('data-id')) {
+        container = container.parent();
+    }
+
+    socket.send(JSON.stringify({
+        header: 'follow-confirm',
+        id_follow: container.attr('data-id'),
+        accept: button.attr('data-action')
+    }));
+
+    $('.item-follow').fadeOut(300);
+    $('.container-follow-push').fadeOut(300);
+};
+
 $('.list-order').click((event) => {
     $('.container-profile').empty();
     $('.container-list-follow').html(`
@@ -15,14 +36,16 @@ $('.list-order').click((event) => {
 
             html.filter((item) => {
                 $('.list-follow').append(`
-                    <div class="item-follow">
+                    <div class="item-follow" data-id=${item.id}>
                     <h2>${item.username}</h2>
                     <div>
-                        <button class="btn btn-success">Принять</button>
-                        <button class="btn btn-danger">Отклонить</button>
+                        <button class="btn btn-success send-follow" data-action="1">Принять</button>
+                        <button class="btn btn-danger send-follow" data-action="0">Отклонить</button>
                     </div>
                     </div>
                 `);
+
+                $('.send-follow').click(confirmFollowHanlder);
             })
         }
     })
