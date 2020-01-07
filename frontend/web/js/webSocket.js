4f2621ea40336ecef0b44cbe4dbc611a3025f7d3
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-07 17:46:22 +0800

    Added events

--- frontend/web/js/webSocket.js
+++ frontend/web/js/webSocket.js
@@ -37,22 +37,41 @@ socket.onmessage = (event) => {
      * Push-уведомление о заявки добавления в друзья
      */
     if (data.header == 'follow-push') {
+        $('.send-follow').click(confirmFollowHanlder);
         $('.container-follow-push').fadeIn(300);
+        $('.container-follow-push').attr('data-id', data.id_order);
         $('.container-follow-push').css('display', 'inline-block');
         $('.follow-push-username').html(data.from);
     }
-}
 
-/**
- * Соединение закрыто
- */
-socket.close = (event) => {
-    console.log('Close');
-}
+    /**
+     * Ответ на заявку в друзья
+     */
+    if (data.header == 'follow-confirm-push') {
+        $('.container-follow-confirm').fadeIn(300);
+        $('.container-follow-confirm').css('display', 'inline-block');
 
-$('.btn-follow').click((event) => {
-    socket.send(JSON.stringify({
-        header: 'follow-push',
-        id_user: $('.btn-follow').attr('href')
-    }))
-})
+        let result = Number(data.accept) ? 'Принял' : 'Отклонил';
+        $('.follow-confirm-message').html(`Пользователь ${data.from} ${result} вашу заявку в друзья`);
+    }
+
+    /**
+     * Соединение закрыто
+     */
+    socket.close = (event) => {
+        console.log('Close');
+    }
+
+    /**
+     * Отправка заявки в друзья
+     */
+    $('.btn-follow').click((event) => {
+        socket.send(JSON.stringify({
+            header: 'follow-push',
+            id_user: $('.btn-follow').attr('href')
+        }))
+
+        $('.btn-follow').html('Заявка в друзья отправлена');
+        $('.btn-follow').toggleClass('btn-follow');
+    })
+}
