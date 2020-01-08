f94c3e4ff006231808a58ef458d6e72ad07009a2
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-08 23:45:34 +0800

    Added methods: actionIndex, actionLastMessage, actionMessage

--- frontend/controllers/ChatController.php
+++ frontend/controllers/ChatController.php
@@ -2,12 +2,55 @@
 
 namespace frontend\controllers;
 
+use Yii;
 use yii\web\Controller;
+use common\models\Message;
 
 class ChatController extends Controller
 {
     public function actionIndex()
     {
-        var_dump('Here');
+        $model = new Message();
+
+        if(Yii::$app->request->isAjax && $model->load(Yii::$app->request->get()))
+        {
+            $model->save();
+            return;
+        }
+
+        return $this->render('index', [
+            'model' => $model
+        ]);
+    }
+
+    /**
+     * Получние последних сообщений
+     * Действие для AJAX запроса
+     */
+    public function actionLastMessage()
+    {
+        if(Yii::$app->request->isAjax)
+        {
+            echo json_encode(Message::getLastMessages(Yii::$app->request->get('pivot')));
+            return;
+        }
+        $this->goHome();
+    }
+
+    /**
+     * Получение сообщений
+     * Действие для AJAX запроса
+     */
+    public function actionMessage()
+    {
+        if(Yii::$app->request->isAjax)
+        {
+            echo json_encode(Message::getMessages(
+                Yii::$app->request->get('hash_user'),
+                Yii::$app->request->get('pivot')
+            ));
+            return;
+        }
+        $this->goHome();
     }
 }
