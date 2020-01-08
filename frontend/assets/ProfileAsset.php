4bd156a13d15b590ba1d11db9e32aad1f57360f2
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-08 23:44:45 +0800

    Update ProfileAsset

--- frontend/assets/ProfileAsset.php
+++ frontend/assets/ProfileAsset.php
@@ -9,7 +9,9 @@ class ProfileAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css',
         'css/site.css',
+        'css/chat.css'
     ];
     public $js = [
         'js/profile.js',
