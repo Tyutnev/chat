dc6fdbb2ab20ae8c3f814aeec3f4d3a4e3a95826
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-12 02:17:58 +0800

    Added logic for getting messages

--- frontend/web/js/chat.js
+++ frontend/web/js/chat.js
@@ -1,3 +1,21 @@
+const getUserId = () => {
+    let idUser;
+
+    $.ajax({
+        type: 'GET',
+        url: 'site/identity',
+        data: {
+            id: 'true'
+        },
+        async: false,
+        success: (html) => {
+            idUser = html;
+        }
+    });
+
+    return idUser;
+};
+
 const getPivot = () => {
     return $($('[data-id]')[$('[data-id]').length - 1]).attr('data-id');
 }
@@ -29,6 +47,53 @@ const getLastMessage = (event) => {
     })
 }
 
+const getMessages = (hash, pivot) => {
+    $.ajax({
+        type: 'GET',
+        url: '/chat/message',
+        data: {
+            hash: hash,
+            pivot: pivot
+        },
+        success: (html) => {
+            html = JSON.parse(html);
+            idUser = getUserId();
+
+            html.filter((item) => {
+                if (idUser == item.id_sender) {
+                    $('.msg_history').append(`
+                        <div class="outgoing_msg">
+                        <div class="sent_msg">
+                        <p>${item.content}</p>
+                        <span class="time_date"> 11:01 AM    |    June 9</span> </div>
+                        </div>
+                    `);
+                } else {
+                    $('.msg_history').append(`
+                        <div class="incoming_msg">
+                        <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                        <div class="received_msg">
+                        <div class="received_withd_msg">
+                            <p>${item.content}</p>
+                            <span class="time_date"> 11:01 AM    |    June 9</span></div>
+                        </div>
+                        </div>`);
+                }
+            })
+        }
+    });
+};
+
 if ($('div').is('.messaging')) {
     getLastMessage();
 }
+
+$('.inbox_chat').click((event) => {
+    let element = $(event.target);
+    let hash;
+    if (hash = element.attr('data-hash')) {
+        $('.msg_history').empty();
+        getMessages(hash);
+    }
+
+});
